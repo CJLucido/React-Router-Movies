bc18aa0ea6916f6be54739122b9dfabcc5e0fa52
CJ Lucido <c.jose.lucido@gmail.com>
2019-10-15 18:11:16 -0700

    fixed uncaught exception <, it was none used space between an opening and closing script

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import {BrowserRouter as Router} from "react-router-dom"
+import {BrowserRouter as Router} from "react-router-dom";
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(<Router><App/></Router>, document.getElementById('root'));
